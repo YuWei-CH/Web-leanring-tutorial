1b7addb30175a4229bd0122487b38842978fab7b
Yuwei <49397791+YuWei-CH@users.noreply.github.com>
2023-04-26 12:52:43 -0400

    React
    add a tutorial example for react

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { useState } from 'react';
+
+function Square({value,onSquareClick}){
+  // value store current value, and setValue store a functio to change value
+  return(
+    <button className="square" onClick={onSquareClick} >{value}</button>
   );
 }
 
-export default App;
+// helper function for winner calculating
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// "export" makes this function accessible outside of this file.
+// "default" keyword tells other files using your code that it’s the main function in your file.
+export default function Board() {
+  // className like id in HTML, here is JSX
+  // return a single JSX
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null)); // set 9 nulls
+
+  function handleclick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice(); //male a copy by .slice()
+    if (xIsNext){
+      nextSquares[i] = "X";
+    }else{
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares); //set copy to the squares
+    setXIsNext(!xIsNext)
+  }
+  // show status on the page
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  return(
+  <>
+
+  <div className="status">{status}</div>
+   <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleclick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleclick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleclick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleclick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleclick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleclick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleclick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleclick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleclick(8)}/>
+      </div>
+  </>
+  );
+}
